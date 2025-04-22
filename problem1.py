db62859b1c1e920b4b050a2ded8ee5412510280d
KDThaGoatt <kyand906@deltalearns.ca>
2025-04-22 11:37:34 -0700

    a

--- problem1.py
+++ problem1.py
@@ -1,3 +1,19 @@
+"""
+Factoring simple trinomials
+Create a user interface using tkinter.
+There should be a label indicating instructions for what the user needs to do.
+The program will factor a trinomial of the type ax^2 + bx + c, where a, b and c
+are coefficients.  For the purposes of this program, a will always be 1.
+The user should enter in coefficients for b and c.  Note that if you are factoring
+a trinomial of the type ax^2 - bx + c, then b is just a negative number.
+There should be a button to factor the trinomial
+The program should display the factored form in an Entry widget.
+
+Extension: make the + between a,b and b,c buttons that will toggle
+between + and -.
+"""
+
+
 import tkinter as tk
 from tkinter import *
 
@@ -42,13 +58,13 @@ def factor(e):
                 if n < 0:
                     posneg4 = True
                 if posneg3 == True:
-                posneg3 = "-"
+                    posneg3 = "-"
                 if posneg3 == False:
-                posneg3 = "+"
+                    posneg3 = "+"
                 if posneg4 == True:
-                posneg4 = "-"
+                    posneg4 = "-"
                 if posneg4 == False:
-                posneg4 = "+"
+                    posneg4 = "+"
                 print(factors)
                 answer = (f"x {posneg3} {factors[0][0]}, x {posneg4} {factors[0][1]}")
                 print(answer)
